34ede07d3669e91cafa83847600d90be75599921
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-08 01:48:39 +0530

    Add C++ solution for Coin_Combinations_II

--- Dynamic_Programming/Coin_Combinations_II/Coin_Combinations_II.cpp
+++ Dynamic_Programming/Coin_Combinations_II/Coin_Combinations_II.cpp
@@ -5,10 +5,49 @@
  * Date: 2026-08-07
  */
 
-input/code.cpp: In function 'long long int sol(int, int)':
-input/code.cpp:12:12: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
-   12 |     if(idx >=  arr.size()) return 0;
-      |        ~~~~^~~~~~~~~~~~~~
-input/code.cpp:21:24: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
-   21 |     for(int i = idx; i < arr.size(); i++){
-      |                      ~~^~~~~~~~~~~~
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+ 
+const int MOD = 1e9 + 7;
+ 
+int n, x;
+vector<int> coins;
+vector<vector<ll>> dp;
+ 
+ll ways(int idx, int sum) {
+    if (sum == x) return 1;
+    if (sum > x) return 0;
+    if (idx == n) return 0;
+ 
+    if (dp[idx][sum] != -1)
+        return dp[idx][sum];
+ 
+    ll ans = 0;
+ 
+    // Take current coin
+    ans += ways(idx, sum + coins[idx]);
+ 
+    // Skip current coin
+    ans += ways(idx + 1, sum);
+ 
+    ans %= MOD;
+ 
+    return dp[idx][sum] = ans;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    cin >> n >> x;
+ 
+    coins.resize(n);
+    for (int &c : coins)
+        cin >> c;
+ 
+    dp.assign(n + 1, vector<ll>(x + 1, -1));
+ 
+    cout << ways(0, 0) << '\n';
+}
