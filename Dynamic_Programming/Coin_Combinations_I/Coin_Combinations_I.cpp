790a04f5e5b166ab23377e55f5933856d718bbe4
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-07 21:51:35 +0530

    Add C++ solution for Coin_Combinations_I

--- Dynamic_Programming/Coin_Combinations_I/Coin_Combinations_I.cpp
+++ Dynamic_Programming/Coin_Combinations_I/Coin_Combinations_I.cpp
@@ -5,7 +5,7 @@
  * Date: 2026-08-07
  */
 
-input/code.cpp: In function 'int sol(int)':
+input/code.cpp: In function 'long long int sol(int)':
 input/code.cpp:21:22: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
    21 |     for(int i = 0; i < arr.size(); i++){
       |                    ~~^~~~~~~~~~~~
