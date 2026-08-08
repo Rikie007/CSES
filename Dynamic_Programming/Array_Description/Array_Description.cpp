e22773c2d1f146d7a41135bd483b5b929865c405
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-08 18:05:26 +0530

    Add C++ solution for Array_Description

---
+++ Dynamic_Programming/Array_Description/Array_Description.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Array_Description
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-08-08
+ */
+
+input/code.cpp: In function 'long long int sol(int, int, std::vector<int>&)':
+input/code.cpp:12:12: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   12 |     if(idx == arr.size()){
+      |        ~~~~^~~~~~~~~~~~~
