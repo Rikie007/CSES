52b9f2597edba86965799ecbee92aa630856c7a7
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-08 17:32:55 +0530

    Add C++ solution for Book_Shop

--- Dynamic_Programming/Book_Shop/Book_Shop.cpp
+++ Dynamic_Programming/Book_Shop/Book_Shop.cpp
@@ -13,26 +13,11 @@ using namespace std;
 #define Rikie cout.tie(NULL);
 #define ll long long
 int N ;
-int t[1001][100001];
-int sol(int i, int cost, vector<int> &price, vector<int> &page){
-    if(i >= N || cost < 0){
-        return 0;
-    }
-    if(t[i][cost] != -1) return t[i][cost];
-    int take = 0;
-    if(price[i] <= cost)
-        take = page[i] + sol(i+1, cost - price[i], price, page);
- 
-    int notTake = sol(i+1, cost, price, page);
- 
-    return t[i][cost] =  max(take, notTake);
-}
 void solve(){
     int n,x;
     cin >> n >> x;
     vector<int> price;
     vector<int> page;
-    N = n;
     for(int i = 0 ; i < n; i++){
         int x;
         cin >> x;
@@ -43,9 +28,17 @@ void solve(){
         cin >> x;
         page.push_back(x);
     }
-    memset(t, -1, sizeof(t));
- 
-    cout << sol(0,x, price, page);
+    vector<int> prev(x+1,0);
+    for(int i = 1; i <= n; i++){
+        vector<int> curr(x+1);
+        for(int j = 0 ; j <= x ; j++){
+            int take = 0;
+            if(j - price[i-1] >= 0) take = page[i-1] + prev[j-price[i-1]];
+            curr[j] = max(prev[j],take);
+        }
+        prev = curr;
+    }
+    cout << prev[x];
 }
  
 int main(){
