e481a3df8c3d1a7f8d5213da70fb825b084f6e24
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-10 17:27:32 +0530

    Add C++ solution for Minimal_Grid_Path

---
+++ Dynamic_Programming/Minimal_Grid_Path/Minimal_Grid_Path.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Minimal_Grid_Path
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-08-10
+ */
+
+input/code.cpp: In function 'void solve()':
+input/code.cpp:38:26: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<std::pair<std::pair<char, int>, int> >::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   38 |         for(int k = 0; k < item.size(); k++){
+      |                        ~~^~~~~~~~~~~~~
