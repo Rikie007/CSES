49696c5486ef96b080ca4b507a4d244510d87694
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-10 14:04:50 +0530

    Add C++ solution for Rectangle_Cutting

--- Dynamic_Programming/Rectangle_Cutting/Rectangle_Cutting.cpp
+++ Dynamic_Programming/Rectangle_Cutting/Rectangle_Cutting.cpp
@@ -12,20 +12,27 @@ using namespace std;
 #define By cin.tie(NULL);
 #define Rikie cout.tie(NULL);
 #define ll long long
- 
+int t[501][501];
 int sol(int a, int b){
     if(a <= 0 || b <= 0) return 0;
     if(a == b){
         return 0;
     }
-    
-    return 1 + sol(a,a) + sol(min(a,b-a),max(a,b-a));
+    if(t[a][b] != -1) return t[a][b];
+    int ans = INT_MAX;
+    for(int i = 1 ; i < a; i++){
+        ans = min(ans, 1 + sol(a-i, b) + sol(i,b));
+    }
+    for(int i = 1; i < b ; i++){
+        ans = min(ans, 1 + sol(a, b-i) + sol(a,i));
+    }
+    return t[a][b] = ans;
 }
  
  
 void solve(){
     int a, b; cin >> a >> b;
- 
+    memset(t,-1,sizeof(t));
     cout <<  sol(min(a,b),max(a,b));
  
 }
