f9e7e13b42a0b8cd48508ee022f4da2fe53960df
Rikie143 <156202573+Rikie007@users.noreply.github.com>
2026-08-10 13:25:19 +0530

    Add C++ solution for Longest_Common_Subsequence

---
+++ Dynamic_Programming/Longest_Common_Subsequence/Longest_Common_Subsequence.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Longest_Common_Subsequence
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-08-10
+ */
+
+input/code.cpp: In function 'void solve()':
+input/code.cpp:71:22: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   71 |     for(int i = 0; i < ans.size(); i++){
+      |                    ~~^~~~~~~~~~~~
